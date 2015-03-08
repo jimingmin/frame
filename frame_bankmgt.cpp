2f9f230ec9d26cd266f266ed42c7555295431b0c
jimingmin <jimingmin@gmail.com>
2015-03-08 23:09:13 +0800

    框架管理器
    用于应用注册并管理各种管理器

---
+++ frame_bankmgt.cpp
@@ -0,0 +1,67 @@
+/*
+ * frame_bankmgt.cpp
+ *
+ *  Created on: Mar 7, 2015
+ *      Author: jimm
+ */
+
+#include "../logger/logger.h"
+#include "frame_typedef.h"
+#include "frame_bankmgt.h"
+
+using namespace LOGGER;
+
+FRAME_NAMESPACE_BEGIN
+
+int32_t CFrameBankMgt::Init()
+{
+	BankMgt::iterator it = m_stBankMgt.begin();
+	for(; it != m_stBankMgt.end(); ++it)
+	{
+		int32_t nRet = it->second->Init();
+		if(nRet != 0)
+		{
+			WRITE_WARN_LOG(MODULE_NAME, "init bank %s error!\n", it->first.c_str());
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int32_t CFrameBankMgt::Uninit()
+{
+	return 0;
+}
+
+int32_t CFrameBankMgt::GetSize()
+{
+	return sizeof(*this);
+}
+
+void CFrameBankMgt::RegistBank(const char *szBankName, IBank *pBank)
+{
+	if(GetBank(szBankName) != NULL)
+	{
+		return;
+	}
+
+	m_stBankMgt[szBankName] = pBank;
+}
+
+IBank *CFrameBankMgt::GetBank(const char *szBankName)
+{
+
+	BankMgt::iterator it = m_stBankMgt.find(szBankName);
+	if(it != m_stBankMgt.end())
+	{
+		return it->second;
+	}
+
+	return NULL;
+}
+
+FRAME_NAMESPACE_END
+
+
+
---
+++ frame_bankmgt.h
@@ -0,0 +1,42 @@
+/*
+ * frame_bankmgt.h
+ *
+ *  Created on: Mar 7, 2015
+ *      Author: jimm
+ */
+
+#ifndef FRAME_BANKMGT_H_
+#define FRAME_BANKMGT_H_
+
+#include "../common/common_object.h"
+#include "frame_impl.h"
+#include "frame_namespace.h"
+#include "frame_bank.h"
+#include <map>
+#include <string>
+using namespace std;
+
+FRAME_NAMESPACE_BEGIN
+
+class CFrameBankMgt : public CBaseObject
+{
+	typedef map<string, IBank *>	BankMgt;
+public:
+	CFrameBankMgt()
+	{
+	}
+
+	virtual int32_t Init();
+	virtual int32_t Uninit();
+	virtual int32_t GetSize();
+
+	void RegistBank(const char *szBankName, IBank *pBank);
+
+	IBank *GetBank(const char *szBankName);
+protected:
+	BankMgt			m_stBankMgt;
+};
+
+FRAME_NAMESPACE_END
+
+#endif /* FRAME_BANKMGT_H_ */
