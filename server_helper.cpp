f7197df5f67d868623f8e68756384aaff788c11b
jimingmin <jimingmin@gmail.com>
2015-03-13 14:55:49 +0800

    服务器辅助类
    1，提供生成通过redis转发的消息buf
    2，提供发送给客户端消息的函数(加密)

---
+++ server_helper.cpp
@@ -0,0 +1,69 @@
+/*
+ * server_helper.cpp
+ *
+ *  Created on: Mar 13, 2015
+ *      Author: jimm
+ */
+
+#include "server_helper.h"
+#include "../common/common_crypt.h"
+#include "../include/typedef.h"
+
+static char g_arrSSKey[16] = {'v', 'd', 'c', '$', 'a', 'u', 't', 'h', '@', '1', '7','9','.', 'c', 'o', 'm'};
+
+int32_t CServerHelper::MakeMsg(ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, uint8_t *pOutBuf, int32_t nOutBufSize)
+{
+//	ControlHead *pControlHead = (ControlHead *)pCtlHead;
+//	MsgHeadCS *pMsgHeadCS = (MsgHeadCS *)pMsgHead;
+
+	uint32_t nOffset = 0;
+	int32_t nRet = pCtlHead->Encode(pOutBuf, nOutBufSize, nOffset);
+	if(nRet != 0)
+	{
+		return 0;
+	}
+
+	nRet = pMsgHead->Encode(pOutBuf, nOutBufSize, nOffset);
+	if(nRet != 0)
+	{
+		return 0;
+	}
+
+	nRet = pMsgBody->Encode(pOutBuf, nOutBufSize, nOffset);
+	if(nRet != 0)
+	{
+		return 0;
+	}
+
+	uint16_t nCtlHeadSize = pCtlHead->GetSize();
+
+	//msghead + msgbody的大小
+	uint16_t nMsgTotalSize = nOffset - nCtlHeadSize;
+	nOffset = nCtlHeadSize;
+	CCodeEngine::Encode(pOutBuf, nOutBufSize, nOffset, nMsgTotalSize);
+
+	nOffset = 0;
+	//ctlhead + msghead + msgbody的大小
+	uint16_t nCtlTotalSize = nMsgTotalSize + nCtlHeadSize;
+	CCodeEngine::Encode(pOutBuf, nOutBufSize, nOffset, nCtlTotalSize);
+
+	return nCtlTotalSize;
+}
+
+int32_t CServerHelper::SendMsgToClient(IIOSession *pIoSession, MsgHeadCS *pMsgHeadCS, uint8_t *pBuf, int32_t nBufSize)
+{
+	uint8_t arrMsg[MAX_MSG_SIZE];
+
+	uint16_t nHeadSize = pMsgHeadCS->GetSize();
+	uint8_t *pBody = pBuf + nHeadSize;
+	int32_t nBodySize = CXTEA::Encrypt((char *)pBody, nBufSize - pMsgHeadCS->GetSize(), (char *)&arrMsg[nHeadSize],
+			sizeof(arrMsg) - nHeadSize, g_arrSSKey);
+
+	memcpy(arrMsg, pBuf, nHeadSize);
+
+	uint16_t nTotalSize = (uint16_t)(nBodySize + pMsgHeadCS->GetSize());
+	uint32_t nOffset = 0;
+	CCodeEngine::Encode(arrMsg, sizeof(arrMsg), nOffset, nTotalSize);
+
+	return pIoSession->Write(arrMsg, nTotalSize);
+}
---
+++ server_helper.h
@@ -0,0 +1,26 @@
+/*
+ * server_helper.h
+ *
+ *  Created on: Mar 12, 2015
+ *      Author: jimm
+ */
+
+#ifndef SERVER_HELPER_H_
+#define SERVER_HELPER_H_
+
+#include "../common/common_codeengine.h"
+#include "../include/control_head.h"
+#include "../include/msg_head.h"
+#include "../netevent/net_impl.h"
+
+using namespace NETEVENT;
+
+class CServerHelper
+{
+public:
+	static int32_t MakeMsg(ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, uint8_t *pOutBuf, int32_t nOutBufSize);
+
+	static int32_t SendMsgToClient(IIOSession *pIoSession, MsgHeadCS *pMsgHeadCS, uint8_t *pBuf, int32_t nBufSize);
+};
+
+#endif /* SERVER_HELPER_H_ */
