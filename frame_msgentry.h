c1c11601c5f38f33b638ab5c328d35721fa58db8
jimingmin <jimingmin@gmail.com>
2015-03-13 14:54:15 +0800

    增加两个消息处理入口类型

--- frame_msgentry.h
+++ frame_msgentry.h
@@ -10,7 +10,7 @@
 
 #include "../common/common_object.h"
 #include "../common/common_typedef.h"
-#include "frame_typedef.h"
+#include "frame_impl.h"
 #include "frame_msghandle.h"
 #include "frame_namespace.h"
 
@@ -52,6 +52,25 @@ public:
 		m_stEntryParam.EP_i32_pco_pmh_pu8_i32.m_pMsgHandleProc = Proc;
 	}
 
+	MsgEntry(CBaseObject *pObj, ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, i32_pch_pmh_pmb_pu8_i32 Proc)
+	{
+		m_nProcCodeFlag = enmProcCodeFlag_i32_pch_pmh_pmb_pu8_i32;
+		m_stEntryParam.EP_i32_pch_pmh_pmb_pu8_i32.m_pObject = pObj;
+		m_stEntryParam.EP_i32_pch_pmh_pmb_pu8_i32.m_pCtlHead = pCtlHead;
+		m_stEntryParam.EP_i32_pch_pmh_pmb_pu8_i32.m_pMsgHead = pMsgHead;
+		m_stEntryParam.EP_i32_pch_pmh_pmb_pu8_i32.m_pMsgBody = pMsgBody;
+		m_stEntryParam.EP_i32_pch_pmh_pmb_pu8_i32.m_pMsgHandleProc = Proc;
+	}
+
+	MsgEntry(CBaseObject *pObj, ICtlHead *pCtlHead, IMsgHead *pMsgHead, i32_pch_pmh_pu8_i32 Proc)
+	{
+		m_nProcCodeFlag = enmProcCodeFlag_i32_pch_pmh_pu8_i32;
+		m_stEntryParam.EP_i32_pch_pmh_pu8_i32.m_pObject = pObj;
+		m_stEntryParam.EP_i32_pch_pmh_pu8_i32.m_pCtlHead = pCtlHead;
+		m_stEntryParam.EP_i32_pch_pmh_pu8_i32.m_pMsgHead = pMsgHead;
+		m_stEntryParam.EP_i32_pch_pmh_pu8_i32.m_pMsgHandleProc = Proc;
+	}
+
 	ProcCodeFlag	m_nProcCodeFlag;
 
 	struct EntryParam_i32_pco_pmh_pmb
@@ -83,13 +102,34 @@ public:
 		i32_pco_pmh_pu8_i32		m_pMsgHandleProc;
 	};
 
+	struct EntryParam_i32_pch_pmh_pmb_pu8_i32
+	{
+		CBaseObject					*m_pObject;
+		ICtlHead				*m_pCtlHead;
+		IMsgHead				*m_pMsgHead;
+		IMsgBody				*m_pMsgBody;
+		i32_pch_pmh_pmb_pu8_i32	m_pMsgHandleProc;
+	};
+
+	struct EntryParam_i32_pch_pmh_pu8_i32
+	{
+		CBaseObject					*m_pObject;
+		ICtlHead				*m_pCtlHead;
+		IMsgHead				*m_pMsgHead;
+		i32_pch_pmh_pu8_i32		m_pMsgHandleProc;
+	};
+
 	union EntryParam
 	{
 		EntryParam_i32_pco_pmh_pmb 			EP_i32_pco_pmh_pmb;
 		EntryParam_i32_pco_pu8_i32			EP_i32_pco_pu8_i32;
 		EntryParam_i32_pco_pmh_pmb_pu8_i32	EP_i32_pco_pmh_pmb_pu8_i32;
 		EntryParam_i32_pco_pmh_pu8_i32		EP_i32_pco_pmh_pu8_i32;
+		EntryParam_i32_pch_pmh_pmb_pu8_i32	EP_i32_pch_pmh_pmb_pu8_i32;
+		EntryParam_i32_pch_pmh_pu8_i32		EP_i32_pch_pmh_pu8_i32;
 	} m_stEntryParam;
+
+
 };
 
 FRAME_NAMESPACE_END
