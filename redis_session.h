988ffaf8825f1462d552c133dd228ecfb9167fd9
jimingmin <jimingmin@gmail.com>
2015-03-17 01:16:55 +0800

    增加若干数据成员的读取设置接口
    SetTimerProc接口是重新设置超时回调函数，及重新设置过期时间的

--- redis_session.h
+++ redis_session.h
@@ -9,19 +9,26 @@
 #define REDIS_SESSION_H_
 
 #include "../common/common_object.h"
+#include "../common/common_datetime.h"
+#include "frame_timer.h"
+#include "frame.h"
 #include <stddef.h>
 
-typedef int32_t (CBaseObject::*HandleRedisReply)(int32_t nResult, void *pReply, void *pSession);
+using namespace FRAME;
+
+typedef int32_t (CBaseObject::*RedisReply)(int32_t nResult, void *pReply, void *pSession);
 
 #define MAX_SESSIONDATA_SIZE		(8 * 1024)
 
 class RedisSession : public CBaseObject
 {
 public:
-	RedisSession(CBaseObject *pHandlerObj, HandleRedisReply Func)
+	RedisSession()
 	{
-		m_pHandlerObj = pHandlerObj;
-		m_pHandleRedisReply = static_cast<HandleRedisReply>(Func);
+		m_nSessionIndex = 0;
+		m_nTimerIndex = 0;
+		m_pHandlerObj = NULL;
+		m_pHandleRedisReply = NULL;
 	}
 
 	virtual ~RedisSession()
@@ -42,12 +49,42 @@ public:
 		return 0;
 	}
 
+	void SetSessionIndex(uint32_t nSessionIndex)
+	{
+		m_nSessionIndex = nSessionIndex;
+	}
+
+	uint32_t GetSessionIndex()
+	{
+		return m_nSessionIndex;
+	}
+
+	void SetTimerIndex(TimerIndex nTimerIndex)
+	{
+		m_nTimerIndex = nTimerIndex;
+	}
+
+	TimerIndex GetTimerIndex()
+	{
+		return m_nTimerIndex;
+	}
+
+	void SetHandlerObj(CBaseObject *pHandlerObj)
+	{
+		m_pHandlerObj = pHandlerObj;
+	}
+
 	CBaseObject *GetHandlerObj()
 	{
 		return m_pHandlerObj;
 	}
 
-	HandleRedisReply GetHandleRedisReply()
+	void SetHandleRedisReply(RedisReply HandleRedisReply)
+	{
+		m_pHandleRedisReply = HandleRedisReply;
+	}
+
+	RedisReply GetHandleRedisReply()
 	{
 		return m_pHandleRedisReply;
 	}
@@ -57,9 +94,21 @@ public:
 		return m_arrSessionData;
 	}
 
+	void SetTimerProc(TimerProc Proc, int32_t nCycleTime)
+	{
+		CTimer *pTimer = g_Frame.GetTimer(m_nTimerIndex);
+		if(pTimer != NULL)
+		{
+			pTimer->SetEndTime(CTimeValue::CurrentTime().Milliseconds() + nCycleTime);
+			pTimer->SetEventProc(Proc);
+		}
+	}
+
 protected:
+	uint32_t			m_nSessionIndex;
+	TimerIndex			m_nTimerIndex;
 	CBaseObject			*m_pHandlerObj;
-	HandleRedisReply	m_pHandleRedisReply;
+	RedisReply			m_pHandleRedisReply;
 	uint8_t				m_arrSessionData[MAX_SESSIONDATA_SIZE];
 };
 
