ab94fc4b8e8f3da0d2ed30a21412447eb16783cd
jimingmin <jimingmin@gmail.com>
2015-03-17 01:15:37 +0800

    redis的网络io对象

--- redis_agent.cpp
+++ redis_agent.cpp
@@ -1,229 +1,40 @@
 /*
  * redis_agent.cpp
  *
- *  Created on: Feb 10, 2015
+ *  Created on: Mar 16, 2015
  *      Author: jimm
  */
 
-#include <stdio.h>
-#include <string.h>
 #include "redis_agent.h"
-#include "redis_glue.h"
-#include "redis_session.h"
 #include "frame.h"
-#include "../logger/logger.h"
-#include "../common/common_memmgt.h"
-
-using namespace LOGGER;
 
 FRAME_NAMESPACE_BEGIN
 
-CRedisAgent::CRedisAgent(int32_t nServerID, char *pAddress, uint16_t nPort)
+CRedisAgent::CRedisAgent()
 {
-	m_pRedisEvtBase = NULL;
-	m_pRedisContext = NULL;
-
-	m_nServerID = nServerID;
-	strcpy(m_arrAddress, pAddress);
-	m_nPort = nPort;
-	m_bConnectSuccess = false;
+	m_pRedisEvtBase = event_base_new();
+	g_Frame.AddRunner(this);
 }
 
 CRedisAgent::~CRedisAgent()
 {
-
+	event_base_free(m_pRedisEvtBase);
 }
 
 int32_t CRedisAgent::Run()
 {
-	if(m_pRedisContext == NULL)
-	{
-		return 0;
-	}
-
-	//return event_base_dispatch(m_pRedisEvtBase);
 	return event_base_loop(m_pRedisEvtBase, EVLOOP_NONBLOCK);
 }
 
-int32_t CRedisAgent::OnConnected()
+void CRedisAgent::AttachAsyncEvent(redisAsyncContext *pContext)
 {
-	return 0;
+	redisLibeventAttach(pContext, m_pRedisEvtBase);
 }
 
-void CRedisAgent::OnClosed()
+void CRedisAgent::DetachAsyncEvent(redisAsyncContext *pContext)
 {
 
-}
-
-int32_t CRedisAgent::Connect()
-{
-	m_pRedisEvtBase = event_base_new();
-	m_pRedisContext = redisAsyncConnect(m_arrAddress, m_nPort);
-	if(m_pRedisContext->err)
-	{
-		WRITE_WARN_LOG(MODULE_NAME, "connect redis error!{error=%s, server_id=%d, address=%s, port=%d}\n",
-				m_pRedisContext->errstr, m_nServerID, m_arrAddress, m_nPort);
-
-		event_base_free(m_pRedisEvtBase);
-		m_pRedisEvtBase = NULL;
-		m_pRedisEvtBase = NULL;
-		return 1;
-	}
-
-	redisLibeventAttach(m_pRedisContext, m_pRedisEvtBase);
-
-	CRedisGlue::RegRedisContext(m_pRedisContext, this);
-
-	g_Frame.AddRunner(this);
-
-	WRITE_INFO_LOG(MODULE_NAME, "connect redis!{server_id=%d, address=%s, port=%d}\n", m_nServerID, m_arrAddress, m_nPort);
-
-	return 0;
-}
-
-void CRedisAgent::Close()
-{
-	redisAsyncDisconnect(m_pRedisContext);
-}
-
-bool CRedisAgent::IsConnected()
-{
-	return m_bConnectSuccess;
-}
-
-void CRedisAgent::Connected(const redisAsyncContext *pContext, int32_t nStatus)
-{
-    if (nStatus != REDIS_OK) {
-		WRITE_WARN_LOG(MODULE_NAME, "connect redis failed!{error=%s, server_id=%d, address=%s, port=%d}\n",
-				pContext->errstr, m_nServerID, m_arrAddress, m_nPort);
-        return;
-    }
-
-    WRITE_INFO_LOG(MODULE_NAME, "connect redis success!{server_id=%d, address=%s, port=%d}\n", m_nServerID, m_arrAddress, m_nPort);
-
-    m_bConnectSuccess = true;
-
-    OnConnected();
-}
-
-void CRedisAgent::Closed(const redisAsyncContext *pContext, int32_t nStatus)
-{
-	OnClosed();
-
-	m_bConnectSuccess = false;
-
-    if (nStatus != REDIS_OK) {
-		WRITE_WARN_LOG(MODULE_NAME, "close redis occur error!{error=%s, server_id=%d, address=%s, port=%d}\n",
-				pContext->errstr, m_nServerID, m_arrAddress, m_nPort);
-
-        event_base_free(m_pRedisEvtBase);
-        CRedisGlue::UnregRedisContext(m_pRedisContext);
-
-        m_pRedisEvtBase = NULL;
-        m_pRedisContext = NULL;
-        return;
-    }
-
-    WRITE_INFO_LOG(MODULE_NAME, "redis connection is closed!{server_id=%d, address=%s, port=%d}\n", m_nServerID, m_arrAddress, m_nPort);
-
-    event_base_free(m_pRedisEvtBase);
-    CRedisGlue::UnregRedisContext(m_pRedisContext);
-
-    m_pRedisEvtBase = NULL;
-    m_pRedisContext = NULL;
-}
-
-void CRedisAgent::OnRedisReply(const redisAsyncContext *pContext, void *pReply, void *pSession)
-{
-	if(pSession == NULL)
-	{
-		return;
-	}
-
-    if (pReply == NULL)
-    {
-    	return;
-    }
-
-    redisReply *pRedisReply = (redisReply *)pReply;
-    RedisSession *pRedisSession = (RedisSession *)pSession;
-    CBaseObject *pHandler = pRedisSession->GetHandlerObj();
-    HandleRedisReply Proc = pRedisSession->GetHandleRedisReply();
-
-    (pHandler->*Proc)(pContext->err, pReply, pRedisSession);
-}
-
-void CRedisAgent::OnUnsubscribeReply(const redisAsyncContext *pContext, void *pReply, void *pSession)
-{
-
-}
-
-int32_t CRedisAgent::SendCommand(const char *szCommand, char *pKey, void *pSession/* = NULL*/)
-{
-	int32_t nStatus;
-	if(pSession == NULL)
-	{
-		nStatus = redisAsyncCommand(m_pRedisContext, NULL, NULL, "%s %b", szCommand, pKey, strlen(pKey));
-	}
-	else
-	{
-		nStatus = redisAsyncCommand(m_pRedisContext, &CRedisGlue::CB_RedisReply, pSession, "%s %b", szCommand, pKey, strlen(pKey));
-	}
-
-	return nStatus;
-}
-
-int32_t CRedisAgent::SendCommand(const char *szCommand, char *szKey, void *pSession, const char *szFormat, va_list ap)
-{
-    char *pCmd;
-    int32_t nLen;
-    int32_t nStatus;
-    nLen = redisvFormatCommand(&pCmd,szFormat,ap);
-
-    /* We don't want to pass -1 or -2 to future functions as a length. */
-    if (nLen < 0)
-        return REDIS_ERR;
-
-    char szCmd[10 * 1024];
-    char *pEnd = strchr(pCmd, '\r');
-    if((pEnd == NULL) && (pCmd != NULL))
-    {
-    	free(pCmd);
-    	return 0;
-    }
-
-    int32_t nIndex = pEnd - pCmd;
-    pCmd[nIndex] = '\0';
-    int32_t nRowCount = atoi(&pCmd[1]);
-    int32_t nCmdLen = sprintf(szCmd, "*%d\r\n$%d\r\n%s\r\n$%d\r\n%s", nRowCount + 2, strlen(szCommand), szCommand, strlen(szKey), szKey);
-    pCmd[nIndex] = '\r';
-    memcpy(&szCmd[nCmdLen], &pCmd[nIndex], nLen - nIndex);
-
-    size_t nTotalSize = nCmdLen + nLen - nIndex;
-    szCmd[nTotalSize] = '\0';
-
-    if(pSession == NULL)
-    {
-    	nStatus = redisAsyncFormattedCommand(m_pRedisContext, NULL, NULL, szCmd, nTotalSize);
-    }
-    else
-    {
-		nStatus = redisAsyncFormattedCommand(m_pRedisContext, &CRedisGlue::CB_RedisReply, pSession, szCmd, nTotalSize);
-    }
-
-    free(pCmd);
-
-    return nStatus;
-}
-
-int32_t CRedisAgent::SendCommand(const char *szCommand, char *szKey, void *pSession, const char *szFormat, ...)
-{
-    va_list ap;
-    va_start(ap, szFormat);
-    int32_t nStatus = SendCommand(szCommand, szKey, pSession, szFormat, ap);
-    va_end(ap);
-
-	return nStatus;
 }
 
 FRAME_NAMESPACE_END
+
--- redis_agent.h
+++ redis_agent.h
@@ -1,7 +1,7 @@
 /*
  * redis_agent.h
  *
- *  Created on: Feb 10, 2015
+ *  Created on: Mar 16, 2015
  *      Author: jimm
  */
 
@@ -10,64 +10,25 @@
 
 #include <stdlib.h>
 #include "../common/common_runnable.h"
-#include "../common/common_object.h"
-#include "../hiredis/hiredis.h"
-#include "../hiredis/async.h"
 #include "../hiredis/adapters/libevent.h"
 #include "frame_namespace.h"
-#include <list>
-using namespace std;
 
 FRAME_NAMESPACE_BEGIN
 
-enum
-{
-	enmMaxInternetAddrLen		= 128,
-};
-
 class CRedisAgent : public IRunnable
 {
-	friend class CRedisGlue;
 public:
-	CRedisAgent(int32_t nServerID, char *pAddress, uint16_t nPort);
+	CRedisAgent();
 
 	virtual ~CRedisAgent();
 
 	virtual int32_t Run();
 
-	virtual int32_t OnConnected();
+	void AttachAsyncEvent(redisAsyncContext *pContext);
 
-	virtual void OnClosed();
-
-	virtual void OnUnsubscribeReply(const redisAsyncContext *pContext, void *pReply, void *pSession);
-
-	int32_t Connect();
-
-	void Close();
-
-	bool IsConnected();
-
+	void DetachAsyncEvent(redisAsyncContext *pContext);
 protected:
-
-	void Connected(const redisAsyncContext *pContext, int32_t nStatus);
-
-	void Closed(const redisAsyncContext *pContext, int32_t nStatus);
-
-	void OnRedisReply(const redisAsyncContext *pContext, void *pReply, void *pSession);
-
-	int32_t SendCommand(const char *szCommand, char *szKey, void *pSession = NULL);
-
-	int32_t SendCommand(const char *szCommand, char *szKey, void *pSession, const char *szFormat, va_list ap);
-
-	int32_t SendCommand(const char *szCommand, char *szKey, void *pSession, const char *szFormat, ...);
-
-private:
-	bool					m_bConnectSuccess;
-	int32_t					m_nServerID;
-	char					m_arrAddress[enmMaxInternetAddrLen];
-	uint16_t				m_nPort;
 	event_base 				*m_pRedisEvtBase;
-	redisAsyncContext		*m_pRedisContext;
 };
 
 FRAME_NAMESPACE_END
